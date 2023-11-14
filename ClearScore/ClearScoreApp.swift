b9d140130b4514d4a2c4a4f05e65967663201765
Edward Downhill <biffdownhill@gmail.com>
2023-11-14 15:10:50 +0000

    Initial Commit

---
+++ ClearScore/ClearScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearScoreApp.swift
+//  ClearScore
+//
+//  Created by Edward Downhill on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClearScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
